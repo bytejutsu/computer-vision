1f63a62b82f737ed1eba46a720bb54aa214f3b35
Dhia Nabli <dhianabli@outlook.com>
2021-10-07 12:38:57 +0200

    drawing rectangles using the mouse dragging

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 import cv2
 import numpy as np
 
+# VARIABLES
+
+# True while mouse button down, False while mouse button UP
+drawing = False
+ix = -1
+iy = -1
+
+
+# FUNCTIONS
+
+
+def draw_rectangle(event, x, y, flags, param):
+
+    global ix, iy, drawing
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+
+        drawing = True
+        ix, iy = x, y
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing:
+            cv2.rectangle(img, (ix, iy), (x, y), (0, 255, 0), -1)
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        cv2.rectangle(img, (ix, iy), (x, y), (0, 255, 0), -1)
 
 def draw_circle(event, x, y, flags, param):
 
@@ -10,10 +37,10 @@ def draw_circle(event, x, y, flags, param):
     elif event == cv2.EVENT_RBUTTONDOWN:
         cv2.circle(img, (x, y), 10, (0, 0, 255), -1)
 
-
+##############################################
 cv2.namedWindow('my_drawing', cv2.WINDOW_GUI_NORMAL)
 
-cv2.setMouseCallback('my_drawing', draw_circle)
+cv2.setMouseCallback('my_drawing', draw_rectangle)
 
 ##############################################
 
