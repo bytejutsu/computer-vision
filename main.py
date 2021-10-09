1c89f0cdfddcd12d79b1800c0066746a877d32f6
Dhia Nabli <dhianabli@outlook.com>
2021-10-09 07:00:28 +0200

    feature matching using brute force

--- main.py
+++ main.py
@@ -2,28 +2,26 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread('DATA/internal_external.png', 0)
 
-contours, hierarchy = cv2.findContours(img, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-
-external_contours = np.zeros(img.shape)
-
-for i in range(len(contours)):
-
-    # EXTERNAL
-    if hierarchy[0][i][3] == -1:
-        cv2.drawContours(external_contours, contours, i, 255, -1)
+def display(img, cmap='gray'):
+    fig = plt.figure(figsize=(12, 10))
+    ax = fig.add_subplot(111)
+    ax.imshow(img, cmap='gray')
+    plt.show()
 
 
-internal_contours = np.zeros(img.shape)
+reeses = cv2.imread('DATA/reeses_puffs.png', 0)
+cereals = cv2.imread('DATA/many_cereals.jpg', 0)
 
-for i in range(len(contours)):
+orb = cv2.ORB_create()
+kp1, des1 = orb.detectAndCompute(reeses, None, )
+kp2, des2 = orb.detectAndCompute(cereals, None, )
 
-    # EXTERNAL
-    if hierarchy[0][i][3] != -1:
-        cv2.drawContours(internal_contours, contours, i, 255, -1)
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
+matches = bf.match(des1, des2)
+matches = sorted(matches, key=lambda x: x.distance)
 
-plt.imshow(internal_contours, cmap='gray')
+reeses_matches = cv2.drawMatches(reeses, kp1, cereals, kp2, matches, None, flags=2)
 
-plt.show()
+display(reeses_matches)
