92e53b0f4be3670ac5bf3ed265a9883c514707ef
Dhia Nabli <dhianabli@outlook.com>
2021-10-09 04:36:16 +0200

    grid detection using circles grid pattern

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-flat_chess = cv2.imread('DATA/flat_chessboard.png')
+dots = cv2.imread('DATA/dot_grid.png')
 
-found, corners = cv2.findChessboardCorners(flat_chess, (7, 7))
+found, corners = cv2.findCirclesGrid(dots, (10, 10), cv2.CALIB_CB_SYMMETRIC_GRID)
 
 print(found)
 
-cv2.drawChessboardCorners(flat_chess, (7, 7), corners, found)
+cv2.drawChessboardCorners(dots, (10, 10), corners, found)
 
-plt.imshow(flat_chess)
+plt.imshow(dots)
 plt.show()
