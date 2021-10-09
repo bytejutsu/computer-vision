afc71fec0809f2ac1b544e00d5edd918f110e442
Dhia Nabli <dhianabli@outlook.com>
2021-10-09 05:12:30 +0200

    contour detection

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import cv2
-import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-dots = cv2.imread('DATA/dot_grid.png')
+img = cv2.imread('DATA/internal_external.png', 0)
 
-found, corners = cv2.findCirclesGrid(dots, (10, 10), cv2.CALIB_CB_SYMMETRIC_GRID)
+contours, hierarchy = cv2.findContours(img, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 
-print(found)
+external_contours = np.zeros(img.shape)
 
-cv2.drawChessboardCorners(dots, (10, 10), corners, found)
+for i in range(len(contours)):
 
-plt.imshow(dots)
+    # EXTERNAL
+    if hierarchy[0][i][3] == -1:
+        cv2.drawContours(external_contours, contours, i, 255, -1)
+
+
+internal_contours = np.zeros(img.shape)
+
+for i in range(len(contours)):
+
+    # EXTERNAL
+    if hierarchy[0][i][3] != -1:
+        cv2.drawContours(internal_contours, contours, i, 255, -1)
+
+
+plt.imshow(internal_contours, cmap='gray')
+
 plt.show()
